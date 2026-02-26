1eeb858fe5de72ba61d30cb321a8494ded80c019
Ed Barlow <edward.m.barlow@gmail.com>
2026-02-26 11:09:26 -0500

    Change frontend port from 3000 to 5174

--- new/backend/app/main.py
+++ new/backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_origins=["http://localhost:5174"],  # React game UI (nginx)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
